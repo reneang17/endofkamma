9710d4a7ec90be67d03af11ee583a107bce03b63
reneang17 <reneang17@gmail.com>
2024-11-10 11:14:05 -0600

    minor changes

--- js/PDFViewer.js
+++ js/PDFViewer.js
@@ -50,10 +50,6 @@ class PDFViewer {
         document.getElementById(this.controls.pageNum).addEventListener('keydown', (event) => this.onPageNumKeydown(event));
         document.getElementById('chapter-select').addEventListener('change', (e) => this.gotoChapter(e.target.value));
         document.getElementById(this.controls.downloadPDF).addEventListener('click', () => this.downloadPDF());
-
-        // Add click and touchend events to canvas for advancing to the next page
-        this.canvas.addEventListener('click', () => this.onCanvasClick());
-        this.canvas.addEventListener('touchend', () => this.onCanvasClick());
     }
 
     onRandomPage() {
@@ -82,11 +78,6 @@ class PDFViewer {
         }
         this.pageNum++;
         this.renderPage(this.pageNum);
-    }
-
-    onCanvasClick() {
-        // Trigger next page functionality when the canvas is clicked or tapped
-        this.onNextPage();
     }
 
     gotoPage() {
