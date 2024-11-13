ea71f3aab1dbfb64a0dc5d5c80812fa3848f128e
reneang17 <reneang17@gmail.com>
2024-11-13 10:38:42 -0600

    Staged all changes

--- js/pic_switcher.js
+++ js/pic_switcher.js
@@ -2,22 +2,14 @@ function setHeaderImage() {
     const headerImage = document.getElementById('headerImage');
     const currentHour = new Date().getHours();
 
-    if (currentHour >= 6 && currentHour < 9) {
-        headerImage.src = "assets/stupa.jpg"; // Replace with your morning image path
+    if (currentHour >= 6 && currentHour < 11) {
+        headerImage.src = "assets/kintsugi.jpeg"; // Replace with your morning image path
     }
     else if (currentHour >= 11 && currentHour < 19) {
         headerImage.src = "assets/kintsugi.jpeg";
     }
     else {
-        headerImage.src = "assets/pexels-shotbyrain-4954713.jpg"; // Replace with your evening image path
-    }
-
-    :root {
-        --darkblue: #F3F3FA;
-        --lightgreen: #592316;
-        --lightgreen: #592316;
-        --darkgray: #592316;
-        --lightgray: #F2F2F2;
+        headerImage.src = "assets/kintsugi.jpeg"; // Replace with your evening image path
     }
 }
 
